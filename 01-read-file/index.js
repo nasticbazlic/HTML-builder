73b081bfe630a2a6400ff31b9198de6550d82a43
Nastya Bazlova <nasticbazlic@gmail.com>
2022-11-07 18:34:38 +0200

    feat: add reading a file with output to the console

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const fs = require('fs')
+fs.readFile(
+    path.join(__dirname, 'text.txt'),
+    'utf-8',
+    (err, data) => {
+        if (err) throw err;
+        console.log(data);
+    }
+);
